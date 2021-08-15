e6602e0b8dcc73bd6e9a818173274232e3c10b60
ssviper <ciscoinstructor@gmail.com>
2021-08-15 05:19:02 -0400

    Update Code

--- DEVASC/TDD EXAMPLE/Iteration2_Parse_Customer_VLANs/test_parser.py
+++ DEVASC/TDD EXAMPLE/Iteration2_Parse_Customer_VLANs/test_parser.py
@@ -1,13 +1,13 @@
 import re
 class ConfigurationParser:
     deviceConfig = open("config.txt".read())
+    def parseCustomerNames(self):...
     def parseCustomerVlan(self, customerName):
         intPattern = (
-            r"interface GigabitEthernet0\/0\. ([0-9]+)\n encapsulation"
+            r"interface GigabitEthernet0\/0\.([0-9]+)\n encapsulation "
             r"dot1Q [0-9]+\n ip vrf forwarding %s"
             % (customerName)
         )
-
         allCustomerSubInterfaces = re.search(intPattern, self.deviceConfig)
         return int(allCustomerSubInterfaces.group(1))
 
@@ -15,8 +15,8 @@ import unittest
 
 class TestParse(unittest.TestCase):
     def test_parse_cust_name(self):...
-    def test_parse_cust_vlan(self):...
-cp = ConfigurationParser()
+    def test_parse_cust_vlan(self):
+        cp = ConfigurationParser()
         customer_name = "CUSTOMER_A"
         expected_vlan = 100
         parsed_vlan = cp.parseCustomerVlan(customer_name)
