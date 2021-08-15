9ec2aefeba112483f484615b8f37a19789ad5381
ssviper <ciscoinstructor@gmail.com>
2021-08-15 04:22:31 -0400

    updated Code

--- DEVASC/TDD EXAMPLE/Iteration2_Parse_Customer_VLANs/test_parser.py
+++ DEVASC/TDD EXAMPLE/Iteration2_Parse_Customer_VLANs/test_parser.py
@@ -1,23 +1,23 @@
 import re
-
 class ConfigurationParser:
-    deviceConfig = open("config.txt", "r").read()
-    def parseCustomerNames(self):...
-    def parseCustomerVlan(self, customerName):...
-    def parseCustomerIPAddress(self, vlan): ...
-        deviceConfig = open("config.txt", "r").read()
-        customerNamePattern = r'ip vrf ([a-zA-Z_]+)\n'
-        customerNames = re.findall(customerNamePattern, self.deviceConfig)
-        return customerNames
+    deviceConfig = open("config.txt".read())
+    def parseCustomerVlan(self, customerName):
+        intPattern = (
+            r"interface GigabitEthernet0\/0\. ([0-9]+)\n encapsulation"
+            r"dot1Q [0-9]+\n ip vrf forwarding %s"
+            % (customerName)
+        )
+
+        allCustomerSubInterfaces = re.search(intPattern, self.deviceConfig)
+        return int(allCustomerSubInterfaces.group(1))
 
 import unittest
 
 class TestParse(unittest.TestCase):
     def test_parse_cust_name(self):...
     def test_parse_cust_vlan(self):...
-    def test_parse_cust_ip_address(self):...
-        cp = ConfigurationParser()
-        expected_names = ['CUSTOMER_A', 'CUSTOMER_B']
-        parsed_names = cp.parseCustomerNames()
-        self.assertEqual(list, type(parsed_names))
-        self.assertEqual(expected_names, parsed_names)
+cp = ConfigurationParser()
+        customer_name = "CUSTOMER_A"
+        expected_vlan = 100
+        parsed_vlan = cp.parseCustomerVlan(customer_name)
+        self.assertEqual(expected_vlan, parsed_vlan)
